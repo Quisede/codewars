57690293000e8675dd18e3087528ae07ff53dad2
Quisede <daniil.druzhin129@yandex.com>
2024-06-06 23:04:06 +0300

    Create count_of_positives.c

---
+++ count_of_positives.c
@@ -0,0 +1,40 @@
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+#include <stddef.h>
+
+void count_positives_sum_negatives(
+  int *values, size_t count, int *positivesCount, int *negativesSum) 
+{
+  *positivesCount = 0;
+  *negativesSum = 0;
+  //если массив пустой
+  if(count == 0){
+    *positivesCount = 0;
+    *negativesSum = 0;
+  }
+  //если в массиве только нули
+  int all_null = 0;
+  for(size_t i = 0; i < count; i++){
+    if(values[i] == 0)
+      all_null += 1;
+  }
+  if(all_null == count){
+    *positivesCount = 0;
+    *negativesSum = 0;
+  }
+  
+  //стандартный массив
+  for(size_t i = 0; i < count; i++){
+    if(values[i] > 0)
+      *positivesCount += 1;
+    else if(values[i] < 0)
+      *negativesSum += values[i];
+  }
+}  
