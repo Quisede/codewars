e0753272bdae8761f2b62a0441f690a0fac23bdf
Quisede <daniil.druzhin129@yandex.com>
2024-06-06 22:38:21 +0300

    Create find_the_smallest_int_in_array.c

---
+++ find_the_smallest_int_in_array.c
@@ -0,0 +1,19 @@
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+#include <stddef.h>
+
+int find_smallest_int(const int array[/* len */], size_t len)
+{
+  signed long min_el = array[0];
+  for(int i = 1; i < len; i++){
+    if(array[i] < min_el){
+      min_el = array[i];
+    }
+  }
+  return min_el;
+}
